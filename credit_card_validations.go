88a98ad87183daca32dfe6af149fd7dac6776682
Sayan Chakraborty <sayanchakraborty@Sayans-Air.station>
2023-11-28 21:40:47 +0100

    Add function to verify if a credit card number is valid

---
+++ credit_card_validations.go
@@ -0,0 +1,51 @@
+package sumUp
+
+import (
+	"fmt"
+	"unicode"
+)
+
+func IsValidCardNumber(cardNumber string) (bool, error) {
+	err := validate(cardNumber)
+	if err != nil {
+		return false, err
+	}
+
+	isSecondDigit := false
+	sumOfAllDigits := 0
+
+	for idx := len(cardNumber) - 1; idx >= 0; idx-- {
+		digit := int(cardNumber[idx] - '0')
+
+		if isSecondDigit {
+			double := digit * 2
+			sumOfAllDigits += (double / 10) + (double % 10)
+		} else {
+			sumOfAllDigits += digit
+		}
+
+		isSecondDigit = !isSecondDigit
+	}
+	return sumOfAllDigits%10 == 0, nil
+}
+
+func validate(cardNumber string) error {
+	if cardNumber == "" {
+		return fmt.Errorf("card number %s contains non digits", cardNumber)
+	}
+
+	if !isDigitsOnly(cardNumber) {
+		return fmt.Errorf("card number %s contains non digits", cardNumber)
+	}
+
+	return nil
+}
+
+func isDigitsOnly(s string) bool {
+	for _, char := range s {
+		if !unicode.IsDigit(char) {
+			return false
+		}
+	}
+	return true
+}
---
+++ credit_card_validations_test.go
@@ -0,0 +1,42 @@
+package sumUp
+
+import (
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestIsValidCardNumber(t *testing.T) {
+	t.Run("should return error for empty card number", func(t *testing.T) {
+		isValid, err := IsValidCardNumber("")
+
+		require.False(t, isValid)
+
+		require.Error(t, err)
+		require.ErrorContains(t, err, "card number  contains non digits")
+	})
+
+	t.Run("should return error for card number with letters in them", func(t *testing.T) {
+		isValid, err := IsValidCardNumber("23352352f345")
+
+		require.False(t, isValid)
+
+		require.Error(t, err)
+		require.ErrorContains(t, err, "card number 23352352f345 contains non digits")
+	})
+
+	t.Run("should return false and no error for card number that is not divisible by 10", func(t *testing.T) {
+		isValid, err := IsValidCardNumber("23352352345")
+
+		require.False(t, isValid)
+
+		require.NoError(t, err)
+	})
+
+	t.Run("should return true and no error for card number that is completely valid", func(t *testing.T) {
+		isValid, err := IsValidCardNumber("5237251624778133")
+
+		require.True(t, isValid)
+
+		require.NoError(t, err)
+	})
+}
